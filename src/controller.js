7017088602ebb9b8d374c3cdc2b3b3c95914930a
hariharen96 <hariharen1996@gmail.com>
2024-12-10 15:58:05 +0530

    chore(deps): add core-js and regenerator-runtime packages

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
 let recipeEl = document.querySelector('.recipe')
 
